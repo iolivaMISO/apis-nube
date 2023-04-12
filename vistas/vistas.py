955b6935e8bf5fa1989da99100cd4c9a3dd6a674
david-andres-leon <da.vargasl1@uniandes.edu.co>
2023-04-11 21:53:29 -0500

    Update vistas.py

--- vistas/vistas.py
+++ vistas/vistas.py
@@ -22,17 +22,29 @@ class VistaSignup(Resource):
 
 class VistaLogin(Resource):
     def post(self):
-        username = request.json["username"]
+        username = request.json.get("username", None)
+        email = request.json.get("email", None)
         password_encriptado = hashlib.md5(
             request.json["password"].encode('utf-8')).hexdigest()
-        usuario = Usuario.query.filter(
-            Usuario.username == username, Usuario.password == password_encriptado).first()
-        db.session.commit()
-        if usuario is None:
-            return {"mensaje": "cuenta no existe"}, 404
+
+        if email is not None:
+            usuario = Usuario.query.filter(
+                Usuario.email == email, Usuario.password == password_encriptado).first()
+            db.session.commit()
+            if usuario is None:
+                return {"mensaje": "cuenta no existe"}, 404
+        elif username is not None:
+            usuario = Usuario.query.filter(
+                Usuario.username == username, Usuario.password == password_encriptado).first()
+            db.session.commit()
+            if usuario is None:
+                return {"mensaje": "cuenta no existe"}, 404
         else:
-            token_acceso = create_access_token(identity=username)
-            return {"token": token_acceso}, 200
+            # Handle the case where email is None
+            return {"mensaje": "correo electrónico no proporcionado"}, 400
+
+        token_acceso = create_access_token(identity=usuario.username)
+        return {"token": token_acceso}, 200
 
 
 class VistaTask(Resource):
@@ -64,7 +76,7 @@ class VistaTasks(Resource):
         if archivo:
             filename = secure_filename(archivo.filename)
             file_name_converted = os.path.splitext(filename)[
-                0]+'.'+new_format
+                                      0] + '.' + new_format
             current_user = Usuario.query.filter(
                 Usuario.username == get_jwt_identity()).first()
             nueva_tarea = Tarea(file_name=filename, file_name_converted=file_name_converted,
@@ -84,10 +96,10 @@ class VistaFiles(Resource):
     @jwt_required()
     def get(self, filename):
         filename = secure_filename(filename)
-        tarea = Tarea.query.filter( Tarea.file_name == filename).first()
+        tarea = Tarea.query.filter(Tarea.file_name == filename).first()
         return send_file(os.path.join(FOLDER_IN, str(tarea.id), filename))
 
 
 def allowed_file(filename):
     return '.' in filename and \
-           filename.rsplit('.', 1)[1].upper() in ALLOWED_EXTENSIONS
+        filename.rsplit('.', 1)[1].upper() in ALLOWED_EXTENSIONS
