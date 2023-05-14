003cc5c74c84429891926ca932f51d306d918d38
David Andres Vargas Leon <da.vargasl1@uniandes.edu.co>
2023-05-13 22:33:08 -0500

    changes

--- WORKER/queue/queue.py
+++ WORKER/queue/queue.py
@@ -17,46 +17,6 @@ from google.cloud import pubsub_v1
 
 queue = Celery('tasks', broker='pubsub://', backend='rpc://')
 
-project_id = 'api-nube-semana-3'
-topic_name = 'my-topic'
-subscriber_name = 'my-subscriber'
-
-topic_name = 'projects/'+ project_id +'/topics/'+topic_name.format(
-    project_id=project_id,
-    topic=topic_name,  # Set this to something appropriate.
-)
-subscription_name = 'projects/'+ project_id +'/subscriptions/'+subscriber_name.format(
-    project_id=project_id,
-    sub=subscriber_name,  # Set this to something appropriate.
-)
-
-#subscriber_max_messages = 10
-
-publisher = pubsub_v1.PublisherClient()
-topic_path = publisher.topic_path(project_id, topic_name)
-
-subscriber = pubsub_v1.SubscriberClient()
-subscription_path = subscriber.subscription_path(project_id, subscriber_name)
-
-# Crear la suscripción si no existe
-#subscriber.create_subscription(request={"name": subscription_path, "topic": topic_path})
-
-
-def callback(message):
-    print(message.data)
-    message.ack()
-
-with pubsub_v1.SubscriberClient() as subscriber:
-    subscriber.create_subscription(
-        name=subscription_name, topic=topic_name)
-    future = subscriber.subscribe(subscription_name, callback)
-    try:
-        future.result()
-    except Exception as e:
-        future.cancel()
-        raise
-
-
 # def enviar_accion():
 #
 #
