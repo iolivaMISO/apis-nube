379c1c05c7990e2c847360b50ce689f489c2dfe2
David Andres Vargas Leon <da.vargasl1@uniandes.edu.co>
2023-05-14 00:03:33 -0500

    Update prueba.py

--- WORKER/queue/prueba.py
+++ WORKER/queue/prueba.py
@@ -17,71 +17,6 @@ topic_name = 'my-topic'
 subscriber_name = 'my-subscriber'
 topic_path = f"projects/{project_id}/topics/{topic_name}"
 
-
-def enviar_accion(id, new_format):
-    process_to_convert(new_format, id)
-    actualizacion_tarea = Tarea.query.filter(Tarea.id == id).first()
-    actualizacion_tarea.status = "processed"
-    db.session.add(actualizacion_tarea)
-    db.session.commit()
-
-
-def callback(message):
-    print("calbackkkkk")
-    print(f"Mensaje recibido: {message.data.decode()}")
-    print("Legoooooooooooooooooooo " + str(message.data.decode()))
-    print(message.data)
-    # Realiza cualquier procesamiento adicional que desees hacer con el mensaje aquí
-    data = str(message.data.decode()).split(",")
-    print(data[0])
-    print(data[1])
-    enviar_accion(data[0], data[1])
-    message.ack()  # Confirma la recepción del mensaje
-
-
-def subscribe():
-    # Crea un cliente de Pub/Sub
-    subscriber = pubsub_v1.SubscriberClient()
-
-    # Crea el nombre completo de la suscripción
-    subscription_path = f"projects/{project_id}/subscriptions/{subscriber_name}"
-
-    # Inicia la suscripción y especifica la función de devolución de llamada
-    streaming_pull_future = subscriber.subscribe(subscription_path, callback=callback)
-
-    # Espera a que la suscripción se mantenga activa
-    try:
-        streaming_pull_future.result()
-    except Exception as e:
-        streaming_pull_future.cancel()
-        raise
-
-
-subscribe()
-
-
-@task_postrun.connect
-def close_session(*args, **kwargs):
-    db.session.remove()
-
-
-def process_to_convert(new_format, nueva_tarea_id):
-    file = get_file_by_id_task(nueva_tarea_id)
-    if new_format.upper() == 'TAR.GZ':
-        convert_file_tar_gz(nueva_tarea_id, file)
-    elif new_format.upper() == '7Z':
-        convert_file_7z(nueva_tarea_id, file)
-    elif new_format.upper() == 'TAR.BZ2':
-        convert_file_tar_bz2(nueva_tarea_id, file)
-
-
-def get_file_by_id_task(id_task):
-    tarea = Tarea.query.get_or_404(id_task)
-
-    with open(tarea.file_path, 'rb') as file:
-        return io.BytesIO(file.read())
-
-
 def convert_file_tar_gz(id_task, file):
     tarea = Tarea.query.get_or_404(id_task)
     # extract the contents of the ZIP file to a temporary directory
@@ -151,3 +86,66 @@ def convert_file_7z(id_task, file):
 
     # delete the temporary directory
     shutil.rmtree(tmp_dir)
+
+def get_file_by_id_task(id_task):
+    tarea = Tarea.query.get_or_404(id_task)
+
+    with open(tarea.file_path, 'rb') as file:
+        return io.BytesIO(file.read())
+
+def process_to_convert(new_format, nueva_tarea_id):
+    file = get_file_by_id_task(nueva_tarea_id)
+    if new_format.upper() == 'TAR.GZ':
+        convert_file_tar_gz(nueva_tarea_id, file)
+    elif new_format.upper() == '7Z':
+        convert_file_7z(nueva_tarea_id, file)
+    elif new_format.upper() == 'TAR.BZ2':
+        convert_file_tar_bz2(nueva_tarea_id, file)
+
+
+def enviar_accion(id, new_format):
+    process_to_convert(new_format, id)
+    actualizacion_tarea = Tarea.query.filter(Tarea.id == id).first()
+    actualizacion_tarea.status = "processed"
+    db.session.add(actualizacion_tarea)
+    db.session.commit()
+
+
+def callback(message):
+    print("calbackkkkk")
+    print(f"Mensaje recibido: {message.data.decode()}")
+    print("Legoooooooooooooooooooo " + str(message.data.decode()))
+    print(message.data)
+    # Realiza cualquier procesamiento adicional que desees hacer con el mensaje aquí
+    data = str(message.data.decode()).split(",")
+    print(data[0])
+    print(data[1])
+    enviar_accion(data[0], data[1])
+    message.ack()  # Confirma la recepción del mensaje
+
+
+def subscribe():
+    # Crea un cliente de Pub/Sub
+    subscriber = pubsub_v1.SubscriberClient()
+
+    # Crea el nombre completo de la suscripción
+    subscription_path = f"projects/{project_id}/subscriptions/{subscriber_name}"
+
+    # Inicia la suscripción y especifica la función de devolución de llamada
+    streaming_pull_future = subscriber.subscribe(subscription_path, callback=callback)
+
+    # Espera a que la suscripción se mantenga activa
+    try:
+        streaming_pull_future.result()
+    except Exception as e:
+        streaming_pull_future.cancel()
+        raise
+
+
+
+@task_postrun.connect
+def close_session(*args, **kwargs):
+    db.session.remove()
+
+
+subscribe()
