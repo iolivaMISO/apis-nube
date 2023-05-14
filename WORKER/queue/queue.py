69dc004c7c40cf8c197cc8e150f3d85f94cd2130
Isai Oliva <i.oliva@uniandes.edu.co>
2023-05-13 22:41:54 -0400

    super worker

--- WORKER/queue/queue.py
+++ WORKER/queue/queue.py
@@ -11,11 +11,25 @@ from ..modelos import Tarea
 from ..app import db
 from celery.signals import task_postrun
 
+import os
+from google.cloud import pubsub_v1
+
+
 queue = Celery('tasks', broker='pubsub://', backend='rpc://')
 
 project_id = 'api-nube-semana-3'
 topic_name = 'my-topic'
 subscriber_name = 'my-subscriber'
+
+topic_name = 'projects/'+ project_id +'/topics/'+topic_name.format(
+    project_id=project_id,
+    topic=topic_name,  # Set this to something appropriate.
+)
+subscription_name = 'projects/'+ project_id +'/subscriptions/'+subscriber_name.format(
+    project_id=project_id,
+    sub=subscriber_name,  # Set this to something appropriate.
+)
+
 #subscriber_max_messages = 10
 
 publisher = pubsub_v1.PublisherClient()
@@ -28,13 +42,29 @@ subscription_path = subscriber.subscription_path(project_id, subscriber_name)
 #subscriber.create_subscription(request={"name": subscription_path, "topic": topic_path})
 
 
-@queue.task(name="queque_envio")
-def enviar_accion(id, new_format):
-    process_to_convert(new_format, id)
-    actualizacion_tarea = Tarea.query.filter(Tarea.id == id).first()
-    actualizacion_tarea.status = "processed"
-    db.session.add(actualizacion_tarea)
-    db.session.commit()
+def callback(message):
+    print(message.data)
+    message.ack()
+
+with pubsub_v1.SubscriberClient() as subscriber:
+    subscriber.create_subscription(
+        name=subscription_name, topic=topic_name)
+    future = subscriber.subscribe(subscription_name, callback)
+    try:
+        print ("despues del print ",future.result())
+    except Exception as e:
+        future.cancel()
+        raise
+
+
+# def enviar_accion():
+#
+#
+#     process_to_convert(new_format, id)
+#     actualizacion_tarea = Tarea.query.filter(Tarea.id == id).first()
+#     actualizacion_tarea.status = "processed"
+#     db.session.add(actualizacion_tarea)
+#     db.session.commit()
 
 
 @task_postrun.connect
