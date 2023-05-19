abd15937669a0ae0036a21a7a72ca2c95ea1385f
Isai Oliva <i.oliva@uniandes.edu.co>
2023-05-19 18:10:19 -0400

    appenginee desployment

--- app.py
+++ app.py
@@ -8,12 +8,12 @@ UPLOAD_FOLDER = '/path/to/the/uploads'
 IP = '10.128.0.7'
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://admin:admin@{IP}:5432/apisnube'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['JWT_SECRET_KEY'] = 'uniandes-cloud-class-2023'
-app.config['PROPAGATE_EXCEPTIONS'] = True
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.debug = True
+#app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://admin:admin@{IP}:5432/apisnube'
+#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#app.config['JWT_SECRET_KEY'] = 'uniandes-cloud-class-2023'
+#app.config['PROPAGATE_EXCEPTIONS'] = True
+#app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+#app.debug = True
 
 
 
